eb22b4d2411cf2cecdcef6385313941bdccaff4e
reubensandwich123 <reubenseet@gmail.com>
2025-07-04 10:54:03 +0000

    Added app.py, testing with testapp.py, added templates requirements, users db, removed my pdf

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cs50-final-project
 A place to store my CS50 final project
+regex to extract data from pdfs
+using re and pdfplumber modules
---
+++ app.py
@@ -0,0 +1,69 @@
+import re
+import pdfplumber
+from flask import Flask, render_template, request, session, redirect
+from flask_session import Session
+import sqlite3
+from werkzeug.security import check_password_hash, generate_password_hash
+
+
+app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+# initialize session
+Session(app)
+
+def get_db():
+    conn = sqlite3.connect("users.db")
+    conn.row_factory = sqlite3.Row  # So results behave like dicts (row["password"])
+    return conn
+
+
+
+
+
+@app.route("/")
+def index():
+    if session.get("user_id") == None:
+        return redirect("/login")
+    return render_template("index.html")
+        
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    session.clear()
+    if request.method == "POST":
+        user_id = request.form.get("user_id")
+        password = request.form.get("password")
+
+        if not user_id or not password:
+            return render_template("error.html", message="Do not leave blanks")
+        
+        db = get_db()
+        cursor = db.execute("SELECT user_id FROM users WHERE user_id = ?", (user_id))
+        user = cursor.fetchone()
+
+        if not user:
+            return render_template('error.html', message="User_id does not exist")
+        
+     
+        if not (check_password_hash((user["password"]), password)): #Wrong password
+            return render_template('error.html')
+        session["user_id"] = user_id
+        return redirect("/")
+
+    
+    return render_template("login.html")
+@app.route("/register", methods=["POST"])
+def register():
+    if request.method == "POST":
+        user_id = request.form.get("user_id")
+        password = request.form.get('password')
+        if not user_id or not password:
+            return render_template("error.html", message="Do not leave blanks!")
+
+        hashed_password = generate_password_hash(password)
+        db = get_db()
+        db.execute("INSERT INTO users (user_id, password) VALUES (?, ?)", (user_id, hashed_password))
+        db.commit()
+        
+        return redirect("/login")
+    return render_template("register.html")
