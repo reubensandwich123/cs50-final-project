1e3333ccaf93a92784971b3eb5a74000d7a2dd69
reubensandwich123 <reubenseet@gmail.com>
2025-07-21 08:04:45 +0000

    Added more stuff:

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from flask_session import Session
 import sqlite3
 from werkzeug.security import check_password_hash, generate_password_hash
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static")
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 # initialize session
